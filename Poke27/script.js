014437f393cbb60e6edceb3a51f3073ff35927e8
RolmanRequena2002 <rolreque2002@gmail.com>
2019-05-31 12:29:52 -0600

    POKEMON

--- Poke27/script.js
+++ Poke27/script.js
@@ -1,9 +1,14 @@
 var pokemon= angular.module("pokemon",[])
 pokemon.controller("pokedex",function($scope,$rootScope,$http){
-	$http({
-		method:"GET",
-		url:"https://pokeapi.co/api/v2/pokemon/35"
-	}).then(function callbackSuccess(y){
-		console.log(y);
-	})
+	$scope.pokedexB= [];
+
+	for (var i = 0; i < 100; i++) {
+		$http({
+			method:"GET",
+			url:"https://pokeapi.co/api/v2/pokemon/"+i
+		}).then(function callbackSuccess(y){
+			console.log(y)
+			$scope.pokedexB.push(y);
+		})
+	}
 });
